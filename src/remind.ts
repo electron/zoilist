c9c87bd8fe2818ec466ac38e847c3670e64300de
Samuel Maddock <smaddock@slack-corp.com>
2024-08-02 23:10:14 -0400

    feat: add tag for first time contributors

--- src/remind.ts
+++ src/remind.ts
@@ -163,6 +163,11 @@ const formatSlackDate = (d: Date) => {
 };
 
 const formatPRListItem = (item: IssueOrPullRequest, activity?: PullRequestActivity) => {
+  const firstTimeContributor = item.author_association === 'FIRST_TIME_CONTRIBUTOR';
+
+  const tags = [firstTimeContributor && ':first-time-contributor:'].filter(Boolean) as string[];
+  const tagsLabel = tags.length ? ` ${tags.join(' ')}` : '';
+
   const createdAt = new Date(item.created_at);
   const reviewLabel = activity
     ? `Last reviewed by @${activity.user?.login} ${timeAgo(activity.created_at)} (${formatSlackDate(
@@ -170,9 +175,8 @@ const formatPRListItem = (item: IssueOrPullRequest, activity?: PullRequestActivi
       )})`
     : `Awaiting review since ${timeAgo(createdAt)} (${formatSlackDate(createdAt)})`;
 
-  return `• *<${item.html_url}|${escapeTitle(item.title)} (#${
-    item.number
-  })>*\n    _${reviewLabel}_`;
+  return `• *<${item.html_url}|${escapeTitle(item.title)} (#${item.number})>*${tagsLabel}
+    _${reviewLabel}_`;
 };
 
 async function main() {
